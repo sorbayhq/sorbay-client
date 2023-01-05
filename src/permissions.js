d29c834622b5b3525503eb5210c34f7553c35eb9
Fukuro192 <salah8elmes.2000@gmail.com>
2023-01-05 08:42:49 +0100

    skip device permissions for other platforms

--- src/permissions.js
+++ src/permissions.js
@@ -1,4 +1,5 @@
 const {systemPreferences} = require("electron")
+const { platform } = require('node:process')
 
 /**
  * Gets the media permissions this app has set.
@@ -9,6 +10,9 @@ const {systemPreferences} = require("electron")
  * @returns {{camera: String, microphone: String, screen: String}}
  */
 const getMediaPermissions = () => {
+  if (platform !== 'win32' || platform !== 'darwin')
+    return {camera: "granted", microphone: "granted", screen: "granted"}
+  
   const types = ["camera", "microphone", "screen"]
   const perms = {}
   for (const type of types) {
