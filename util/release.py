e976d98a3405b159fc141b257d79e95ef102ce2b
Jannis Gebauer <ja.geb@me.com>
2023-01-16 13:26:19 +0100

    new release 0.0.21

--- util/release.py
+++ util/release.py
@@ -7,16 +7,16 @@ with open("package.json", "r") as f:
 data['version'] = input(f"version (current {data['version']}): ")
 print(f"writing new version {data['version']} to package.json")
 
-# with open("package.json", "w") as f:
-#     f.write(json.dumps(data, indent=2))
-#
-#
-# print("commiting, tagging & pushing release..")
-# subprocess.run("git checkout master", shell=True)
-# subprocess.run("git add .", shell=True)
-# subprocess.run(f"git commit -a -m 'new release {data['version']}'", shell=True)
-# subprocess.run(f"git tag v{data['version']}", shell=True)
-# subprocess.run("git push origin master --tags", shell=True)
+with open("package.json", "w") as f:
+    f.write(json.dumps(data, indent=2))
+
+
+print("commiting, tagging & pushing release..")
+subprocess.run("git checkout master", shell=True)
+subprocess.run("git add .", shell=True)
+subprocess.run(f"git commit -a -m 'new release {data['version']}'", shell=True)
+subprocess.run(f"git tag v{data['version']}", shell=True)
+subprocess.run("git push origin master --tags", shell=True)
 
 print("creating github release")
 subprocess.run(f"gh release --repo sorbayhq/sorbay-client create v{data['version']} --draft --title 'v{data['version']}' --notes 'release {data['version']}'", shell=True)
